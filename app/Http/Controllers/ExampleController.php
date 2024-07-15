19ef3e771b8deeb63344162bc98c022e0265970c
rasha-saber <rashasaber808@gmail.com>
2024-07-15 23:14:09 +0300

    Add files via upload

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function index()
+    {
+        return view('contact');
+    }
+
+    public function contactsubmit(Request $request)
+    {
+    //     // return $request->all();
+    //     // // return "form submitted";
+    
+        $name=$request->input('name');
+        $email = $request->input('email');
+        $subject = $request->input('subject');
+        $message = $request->input('message');
+       
+        return 'Name : '.$name . 'Email : '. $email . 'Subject : '.$subject .  'Message : '. $message;
+    }
+}
+
