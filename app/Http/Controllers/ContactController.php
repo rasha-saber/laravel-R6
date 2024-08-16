cc081e63f3c7253aa56d17d25a1a272a4cec9223
RashaSaber <rashsaber19@gmail.com>
2024-08-16 23:19:20 +0300

    TASK12

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Mail\ContactMail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
@@ -10,28 +9,27 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact');
-    }
+         return view('contact');
+    } 
 
-    public function sendContact(Request $request)
-    {
+    public function sendContact(Request $request){
 
 
 
 
         $data = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'subject' => 'required|string|max:255',
-            'message' => 'required|string',
-        ]);
-        // $data = [
-        //     'name' => $request->name,
-        //     'email' => $request->email,
-        //     'subject' => $request->subject,
-        //     'message' => $request->message,
-        // ];
-        Mail::to('rashasaer199@gmail.com')->send(new ContactMail($data));
-        return back()->with('success', 'your message has been sent successfully');
-    }
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'subject' => 'required|string|max:255',
+        'message' => 'required|string',
+    ]);
+    // $data = [
+    //     'name' => $request->name,
+    //     'email' => $request->email,
+    //     'subject' => $request->subject,
+    //     'message' => $request->message,
+    // ];
+    Mail::to('rashasaer199@gmail.com')->send(new ContactMail($data));
+    return back()->with('success', 'your message has been sent successfully');
+}
 }
